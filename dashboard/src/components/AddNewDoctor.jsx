18967400db1b1585c64c32c56ed2ff1cdf545891
Harsh <harshkhandelwal2525@gmail.com>
2024-10-05 01:58:09 +0530

    AddNewAdmin Completed

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -1,11 +1,87 @@
 import React from 'react'
-
+import { useState,useContext,useEffect } from 'react';
+import axios from 'axios';
+import { Navigate,useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { Context } from '../main.jsx';
 const AddNewDoctor = () => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [email,setEmail] = useState("");
+  const [phone,setPhone] = useState("");
+  const [nic,setNic] = useState("");
+  const [dob,setDob] = useState("");
+  const [gender,setGender] = useState("");
+  const [password,setPassword] = useState("");
+ const [doctorDepartment,setDoctorDepartment] = useState("");
+ const [docAvatar,setDocAvatar] = useState("");
+ const [docAvatarPreview,setDocAvatarPreview] = useState("");
+  const navigate = useNavigate();
+const handleAddNewDoctor = async (e) =>
+{
+      e.preventDefault();
+      try {
+        const response = await axios.post("http://localhost:4000/api/v1/user/doctor/addnew",{firstName,lastName,email,phone,nic,dob,gender,password,},{
+         withCredentials: true,
+         headers : {"Content-Type": "application/json"},
+        });
+        toast.success(response.data.message);
+       setIsAuthenticated(true);
+        console.log(isAuthenticated);
+        navigate("/"); 
+        
+   } catch (error) {
+     
+     toast.error(error.response.data.message);
+   }
+};
+if(!isAuthenticated)
+{
+  return <Navigate to={"/login"}></Navigate>;
+}
   return (
-    <div>
-      
+    <>
+     <section className='page'>
+     <div className='container form-component register-form'>
+      <img src="/logo.png" alt="logo" className='logo'/>
+       <h1 className='form-title'>ADD NEW ADMIN</h1>
+    
+       <form onSubmit={handleAddNewDoctor}>
+        <div>
+          <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} />
+
+        </div>
+        <div>
+          <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="number" placeholder='Phone No.' value={phone} onChange={(e) => setPhone(e.target.value)} />
+
+        </div>
+        <div>
+          <input type="number" placeholder='NIC' value={nic} onChange={(e) => setNic(e.target.value)} />
+          <input type="date" placeholder='DOB' value={dob} onChange={(e) => setDob(e.target.value)} />
+
+        </div>
+        <div>
+             <select value={gender} onChange={(e) => setGender(e.target.value)}>
+               <option value="">Select Gender</option>
+               <option value="Male">Male</option>
+               <option value="Female">Female</option>
+
+             </select>
+          <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        </div>
+
+  <div style={{justifyContent: "center" , alignItems :"center"}}>
+ <button type="submit">Add New Admin</button>
+  </div>
+       </form>
     </div>
+      </section> 
+    </>
   )
 }
 
-export default AddNewDoctor
+export default AddNewDoctor;
--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
-
+import {Context} from "../main.jsx";
+import{ useContext }from "react";
+import { useState,useEffect, } from 'react';
+import { Navigate } from 'react-router-dom';
+import axios from "axios";
 const Messages = () => {
-  
+  const [messages,setMessages] = useState([]);
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  useEffect(() =>{
+    const fetchMessages = async()=>{
+      try {
+        const {data} = await axios.get("http://localhost:4000/api/v1/message/getAllMessages",{withCredentials:true});
+        setMessages(data.messages);
+      } catch (error) {
+        console.log("Error Occured while fetching Messages :",error);
+      }
+    };
+    fetchMessages();
+  },[]);
+  if(!isAuthenticated)
+  {
+    return <Navigate to={"/login"}></Navigate>
+  }
   return (
-    <div>
-      
-    </div>
+    <section className='page messages'>
+      <h1>MESSAGES</h1>
+      <div className='banner'>
+        {
+          messages && messages.length > 0?(messages.map(element =>{
+            return(
+              <div className='card'>
+                  <div  className='details'>
+                      <p>First Name: <span>{element.firstName}</span></p>
+                      <p>Last Name: <span>{element.lastName}</span></p>
+                      <p>Phone: <span>{element.phone}</span></p>
+                      <p>Message: <span>{element.message}</span></p>
+                      <p></p>
+                  </div>
+              </div>
+            )
+          })) : (<h1>No Messages!</h1>)
+        }
+      </div>
+    </section>
   )
 }
 
